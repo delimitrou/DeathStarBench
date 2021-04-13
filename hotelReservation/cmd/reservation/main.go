280319a81cd29c5bbb890733aea1d297cd20b587
Mohammad Reza Saleh <mr.salehsedghpour@gmail.com>
2021-04-13 21:43:22 +0200

    make reservation able to work with kubernetes

--- hotelReservation/cmd/reservation/main.go
+++ hotelReservation/cmd/reservation/main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
+	"strings"
 
 	"github.com/harlow/go-micro-services/registry"
 	"github.com/harlow/go-micro-services/services/reservation"
@@ -29,26 +31,49 @@ func main() {
 
 	var result map[string]string
 	json.Unmarshal([]byte(byteValue), &result)
+	serv_port, _ := strconv.Atoi(result["ReservePort"])
+	serv_ip := ""
+	reserve_mongo_addr := ""
+	reserve_memc_addr := ""
+	jaegeraddr := flag.String("jaegeraddr", "", "Jaeger address")
+	consuladdr := flag.String("consuladdr", "", "Consul address")
 
-	mongo_session := initializeDatabase(result["ReserveMongoAddress"])
+	if result["Orchestrator"] == "k8s"{
+		reserve_mongo_addr = "mongodb-reserve:"+strings.Split(result["ReserveMongoAddress"], ":")[1]
+		reserve_memc_addr = "memcached-reserve:"+strings.Split(result["ReserveMemcAddress"], ":")[1]
+		addrs, _ := net.InterfaceAddrs()
+		for _, a := range addrs {
+			if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					serv_ip = ipnet.IP.String()
+
+				}
+			}
+		}
+		*jaegeraddr = "jaeger:"+strings.Split(result["jaegerAddress"], ":")[1]
+		*consuladdr = "consul:" + strings.Split(result["consulAddress"], ":")[1]
+	} else {
+		reserve_mongo_addr = result["ReserveMongoAddress"]
+		reserve_memc_addr = result["ReserveMemcAddress"]
+		serv_ip = result["ReserveIP"]
+		*jaegeraddr = result["jaegerAddress"]
+		*consuladdr = result["consulAddress"]
+	}
+	flag.Parse()
+
+	mongo_session := initializeDatabase(reserve_mongo_addr)
 	defer mongo_session.Close()
 
 	fmt.Printf("reservation memc addr port = %s\n", result["ReserveMemcAddress"])
-	memc_client := memcache.New(result["ReserveMemcAddress"])
+	memc_client := memcache.New(reserve_memc_addr)
 	memc_client.Timeout = time.Second * 2
 	memc_client.MaxIdleConns = 512
 
-	serv_port, _ := strconv.Atoi(result["ReservePort"])
-	serv_ip   := result["ReserveIP"]
+
 
 	fmt.Printf("reservation ip = %s, port = %d\n", serv_ip, serv_port)
 
-	var (
-		// port       = flag.Int("port", 8087, "The server port")
-		jaegeraddr = flag.String("jaegeraddr", result["consulAddress"], "Jaeger server addr")
-		consuladdr = flag.String("consuladdr", result["consulAddress"], "Consul address")
-	)
-	flag.Parse()
+
 
 	tracer, err := tracing.Init("reservation", *jaegeraddr)
 	if err != nil {
