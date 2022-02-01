24cf925eb891354a31134c6d5f23878d71015981
Igor Rudyk <rudyk.igor@gmail.com>
2022-02-01 17:13:28 +0200

    Build multiarch Docker images implementation

--- hotelReservation/kubernetes/scripts/build-docker-images.sh
+++ hotelReservation/kubernetes/scripts/build-docker-images.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+#####################################
+### Build multiarch Docker images ###
+#####################################
 
 cd $(dirname $0)/..
 
 
-EXEC=docker
+EXEC="docker buildx"
 
-USER="aabuddabi"
+USER="igorrudyk1"
 
 TAG="latest"
 
 # ENTER THE ROOT FOLDER
 cd ../
 ROOT_FOLDER=$(pwd)
+$EXEC create --name mybuilder --use
+
 for i in frontend geo profile rate recommendation reserve search user
 do
   IMAGE=hotel_reserv_${i}_single_node
   echo Processing image ${IMAGE}
   cd $ROOT_FOLDER
-  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile .
-  $EXEC push "$USER"/"$IMAGE":"$TAG"
+  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/arm64,linux/amd64 --push
   cd $ROOT_FOLDER
 
   echo
