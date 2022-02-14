d99fe1fd30b1354e67b2ba5162a9209b041ab26c
Igor Rudyk <rudyk.igor@gmail.com>
2022-02-14 14:54:40 +0200

    Multi-architecture Docker image implementation

--- hotelReservation/kubernetes/scripts/build-docker-images.sh
+++ hotelReservation/kubernetes/scripts/build-docker-images.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)/..
 
 
-EXEC=docker
+EXEC="docker buildx"
 
 USER="igorrudyk1"
 
@@ -12,14 +12,14 @@ TAG="latest"
 # ENTER THE ROOT FOLDER
 cd ../
 ROOT_FOLDER=$(pwd)
+$EXEC create --name mybuilder --use
 
 for i in hotelreservation #frontend geo profile rate recommendation reserve search user #uncomment to build multiple images
 do
   IMAGE=${i}
   echo Processing image ${IMAGE}
   cd $ROOT_FOLDER
-  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile .
-  $EXEC push "$USER"/"$IMAGE":"$TAG"
+  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/arm64,linux/amd64 --push
   cd $ROOT_FOLDER
 
   echo
