7c8ddb1903106c1ef9be3cc742609cc2a7e25c9c
Nathan Zhou <zzhou612@gmail.com>
2022-03-18 16:18:16 -0400

    add wrk2 scripts with determined parameters

---
+++ socialNetwork/wrk2/scripts/social-network-determinism/read-home-timeline.lua
@@ -0,0 +1,21 @@
+require "socket"
+local time = socket.gettime()*1000
+math.randomseed(time)
+math.random(); math.random(); math.random()
+
+-- load env vars
+local max_user_index = tonumber(os.getenv("max_user_index")) or 962
+local start = tostring(tonumber(os.getenv("start")) or 0)
+local stop = tostring(tonumber(os.getenv("stop")) or 10)
+
+request = function()
+  local user_id = tostring(math.random(1, max_user_index))
+
+  local args = "user_id=" .. user_id .. "&start=" .. start .. "&stop=" .. stop
+  local method = "GET"
+  local headers = {}
+  headers["Content-Type"] = "application/x-www-form-urlencoded"
+  local path = "http://localhost:8080/wrk2-api/home-timeline/read?" .. args
+  return wrk.format(method, path, headers, nil)
+
+end
