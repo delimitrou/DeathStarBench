5c78108d079c5aafc257b2e9b3d33141afd39d9b
wolski0420 <wolski0420@gmail.com>
2022-06-22 22:16:22 +0200

    Applying required change for swarm initialization

--- reactive-company/docker-swarm.sh
+++ reactive-company/docker-swarm.sh
@@ -4,7 +4,7 @@ set -e
 
 ## Enable swarm mode.
 ## Other nodes can join this swarm cluster and this would easily allow to deploy the multi-container application to a multi-host as well.
-docker swarm init
+docker swarm init --advertise-addr 127.0.0.1
 ## Deploy the services defined in Compose file.
 docker stack deploy --compose-file=docker-compose.yml stack
 
