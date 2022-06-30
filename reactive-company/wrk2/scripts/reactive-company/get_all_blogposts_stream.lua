904d338e80f42cb8e2d91d52111f2f797297efc0
wolski0420 <wolski0420@gmail.com>
2022-06-30 02:29:22 +0200

    Provided stream test and added some "done" politics, implemented runner to generate some data

--- reactive-company/wrk2/scripts/reactive-company/get_all_blogposts_stream.lua
+++ reactive-company/wrk2/scripts/reactive-company/get_all_blogposts_stream.lua
@@ -1,20 +1,21 @@
 local socket = require("socket")
 math.randomseed(socket.gettime()*1000)
-math.random(); math.random(); math.random()
 
 
-local function get_all_blogposts()
+local function get_all_blogposts_stream()
     local method = "GET"
     local headers = {}
     headers["Content-Type"] = "application/json"
 
-    local path = "http://localhost:8080/blogposts"
-
+    --choose endpoint for stream
+    local path = "http://localhost:8080/stream"
+    -- local path = "http://localhost:8080/stream/blog"
     return wrk.format(method, path, headers, nil)
 end
 
+
 request = function()
-    return get_all_blogposts()
+    return get_all_blogposts_stream()
 end
 
 
